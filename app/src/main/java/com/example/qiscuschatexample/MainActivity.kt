08982b0f21757855cd8353dc009dc1bee9ec382c
Wahyu Dwe <wahyudwihartarto3@gmail.com>
2022-08-08 20:52:50 +0700

    clean code

--- app/src/main/java/com/example/qiscuschatexample/MainActivity.kt
+++ app/src/main/java/com/example/qiscuschatexample/MainActivity.kt
@@ -21,51 +21,55 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val qiscusCore = QiscusCore()
-        val appId: String = "gane-7jdfmmr9i2vwwbd1" // change with your AppId
-        val localKey: String = "amikomone_channel" // change with your localKey
+        val appId = "gane-7jdfmmr9i2vwwbd1" // change with your AppId
+        val localKey = "amikomone_channel" // change with your localKey
         qiscusMultichannelWidget = QiscusMultichannelWidget.setup(
             application,
             qiscusCore,
             appId,
-//            color,
             localKey
         )
-        qiscusMultichannelWidget.setUser(userId = "1", "Dummy User", avatar = "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=740&t=st=1659946112~exp=1659946712~hmac=723d56fc2add5504db8395b4d084c0675c2c178cd89eb562faabc807e87dbb01")
-        initChat(125948)
+        qiscusMultichannelWidget.setUser(
+            userId = "1",
+            "Dummy User",
+            avatar = "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=740&t=st=1659946112~exp=1659946712~hmac=723d56fc2add5504db8395b4d084c0675c2c178cd89eb562faabc807e87dbb01"
+        )
+        initChat()
     }
 
-    private fun initChat(channelId: Int) {
+    private fun initChat() {
         qiscusMultichannelWidget.initiateChat()
             .showLoadingWhenInitiate(false)
-            .setChannelId(channelId) // manual set channels id
-            .setRoomTitle("Dummy Title")
+            .setChannelId(125948)
+            .setRoomTitle("Custom Title")
             .setAvatar(QiscusMultichannelWidgetConfig.Avatar.DISABLE)
             .setRoomSubtitle(
                 QiscusMultichannelWidgetConfig.RoomSubtitle.EDITABLE,
-                "Dummy subtitle"
+                "Custom Subtitle"
             )
             .setShowSystemMessage(true)
             .setSessional(false)
             .startChat(this, object : QiscusChatRoomBuilder.InitiateCallback {
-                override fun onProgress() {
-                    Log.i("InitiateCallback", "onProgress: ")
-                }
-
                 override fun onSuccess(qChatRoom: QChatRoom) {
-                    Log.i("InitiateCallback", "onSuccess: ")
+                    Log.i("InitialCallback", "onSuccess")
                     qiscusMultichannelWidget.openChatRoomById(
                         this@MainActivity,
                         qChatRoom.id,
                         true
-                    ) { throwable ->
+                    ){ throwable ->
                         throwable.printStackTrace()
                     }
                 }
 
-                override fun onError(throwable: Throwable) {
-                    Log.e("InitiateCallback", "onError: ${throwable.message}")
-
+                override fun onProgress() {
+                    Log.i("InitialCallback", "onProgress")
                 }
+
+                override fun onError(throwable: Throwable) {
+                    Log.i("InitialCallback", "onError: ${throwable.message}")
+                }
+
             })
-        }
+
     }
+}
